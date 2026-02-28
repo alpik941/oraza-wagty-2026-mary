e634a4041cd42a82934195df10650c1361cc9956
alpik941 <alparslankurtoglu941@gmail.com>
2026-02-28 06:33:31 +0300

    Add base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
   return {
+    // 🔹 Добавляем base для GitHub Pages
+    base: '/oraza-wagty-2026---mary(1)/', // <- сюда название репозитория на GitHub
+
     server: {
       port: 3000,
       host: '0.0.0.0',
